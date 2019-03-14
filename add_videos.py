b2ea17ac42ed4d1bcd3d14deb545f17a20fb5d07
test <test>
2019-03-14 12:46:52 +0000

    Set video duration using moviepy, convert with ffmpeg automatically

--- add_videos.py
+++ add_videos.py
@@ -1,45 +1,59 @@
 #!/usr/bin/env python3
 import dataset
 import os
-import shutil
 import sys
 
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
+ffmpeg_opts = '-vf scale=-2:360 -c:v libx264 -preset veryfast -c:a aac'
+
 
 # https://www.reddit.com/r/moviepy/comments/2bsnrq/is_it_possible_to_get_the_length_of_a_video/
 def getVideoLength(filename):
-    return VideoFileClip(filename).duration * 60  # Seconds to milliseconds
+    return VideoFileClip(filename).duration * 1000  # Seconds to milliseconds
+
+
+# https://askubuntu.com/revisions/867858/2
+def convertVideo(video, output):
+    return os.system(
+        f'ffmpeg -i {video} {ffmpeg_opts} {output}'
+    )
 
 
 # Mount IPod
+mounted = False  # Stores wether program mount the IPod or not
 ipod_mount_path = '/tmp/IPod'
 if not os.path.exists(ipod_mount_path):
     os.mkdir(ipod_mount_path)
 if not os.listdir(ipod_mount_path):
     out = os.system('ifuse ' + ipod_mount_path)
+    mounted = True
 
 # Make "Videos" Folder if necessary
 itunes_path = f'{ipod_mount_path}/iTunes_Control/Music/Videos/'
 if not os.path.isdir(itunes_path):
     os.mkdir(itunes_path)
 
-itunes_db_path = '{ipod_mount_path}/iTunes_Control/iTunes/iTunes Library.itlp'
+itunes_db_path = f'{ipod_mount_path}/iTunes_Control/iTunes/iTunes Library.itlp'
 location_db = dataset.connect(
-    f'sqlite+pysqlite://{itunes_db_path}/Locations.itdb'
+    f'sqlite+pysqlite:///{itunes_db_path}/Locations.itdb'
 )
 library_db = dataset.connect(
-    f'sqlite+pysqlite://{itunes_db_path}/Library.itdb'
+    f'sqlite+pysqlite:///{itunes_db_path}/Library.itdb'
 )
 
 for video_path in sys.argv[1:]:
     video_filename = os.path.split(video_path)[1]
 
     # Copy video to necessary location
+    print(f'Copying and converting {video_filename}')
     video_location = itunes_path + video_filename
-    shutil.copyfile(video_path, video_location)
+    if os.path.exists(video_location):
+        print('Video already exists. Skipping.')
+    else:
+        convertVideo(video_path, video_location)
 
-    print(f'Adding {video_filename}')
+    print(f'Adding {video_filename} to database')
 
     # (location database) Use last row but change necessary fields
     last = location_db['location'].find_one(order_by=['-item_pid'])
@@ -55,10 +69,11 @@ for video_path in sys.argv[1:]:
 
     last['pid'] += 1
     last['title'] = os.path.splitext(video_filename)[0]
-    last['total_time_ms'] = getVideoLength(video_filename)
+    last['total_time_ms'] = getVideoLength(video_path)
 
     # (library database) Add modified item
     library_db['item'].insert(last)
 
-# Unmount IPod
-out = os.system('fusermount -u /tmp/IPod')
+# Unmount IPod if the program mounted it
+if mounted:
+    out = os.system('fusermount -u /tmp/IPod')
